c2703a53c8a9a57624c8cff7f8f0522a37a029ca
Steve Clement <steve@localhost.lu>
2014-12-14 21:26:23 +0100

    - Some refactoring and added serial debug

--- H4K_PoV_bar/H4K_PoV_bar.ino
+++ H4K_PoV_bar/H4K_PoV_bar.ino
@@ -10,6 +10,7 @@ int buttonState = 0;
 int lastButtonState = 0;     // previous state of the button
 int buttonPushCounter = 0;   // counter for the number of button presses
 int brightness = 128;
+int wait = 150;
 int j;
 
 unsigned long red = pixel_strip.Color(255, 0, 0);
@@ -18,12 +19,16 @@ unsigned long blue = pixel_strip.Color(0, 0, 255);
 unsigned long white = pixel_strip.Color(255, 255, 255);
 unsigned long lightBlue = pixel_strip.Color(0, 216, 230);
 
+int serialDebug = 1;
+
 void setup() {
   pixel_strip.begin(); // This is a comment BTW.
   pixel_strip.show(); // Initialize all pixels to 'off'
   pixel_strip.setBrightness(brightness);
   pinMode(buttonPin, INPUT_PULLUP);
-  Serial.begin(9600);
+  if (serialDebug) {
+    Serial.begin(9600);
+  }
   startUp();
 }
 
@@ -37,55 +42,69 @@ void loop() { // this is the main loop
       // if the current state is HIGH then the button
       // wend from off to on:
       buttonPushCounter++;
-      Serial.println("on");
-      Serial.print("number of button pushes:  ");
-      Serial.println(buttonPushCounter);
+      if (serialDebug) {
+        Serial.println("on");
+        Serial.print("number of button pushes:  ");
+        Serial.println(buttonPushCounter);
+      }
     } 
     else {
       // if the current state is LOW then the button
       // went from on to off:
-      Serial.println("off"); 
+      if (serialDebug) {
+        Serial.println("off"); 
+      }
     }
   }
   // save the current state as the last state, 
   //for next time through the loop
   lastButtonState = buttonState;
 
-  
   // turns on the LED every four button pushes by 
   // checking the modulo of the button push counter.
   // the modulo function gives you the remainder of 
   // the division of two numbers:
-  if (buttonPushCounter % 2 == 0) {
+  if (buttonPushCounter) {
          for (j=0; j < 8; j++) {
-          pixel_strip.setPixelColor(j, red);
+           pixel_strip.setPixelColor(j, red);
          }
-
-          Serial.println("r");
-          Serial.println(buttonPushCounter);
-
-            pixel_strip.show();
-          delay(250);
-  } 
-
-  if (buttonPushCounter % 3 == 0) {
-         for (j=0; j < 8; j++) {
-          pixel_strip.setPixelColor(j, green);
+         if (serialDebug) {
+           Serial.println("r");
+           Serial.println(buttonPushCounter);
          }
-         Serial.println("g");
          pixel_strip.show();
-
-          delay(250);
+         delay(wait);
   } 
 
-  if (buttonPushCounter % 4 == 0) {
+  if (buttonPushCounter == 2) {
          for (j=0; j < 8; j++) {
-          pixel_strip.setPixelColor(j, blue);
+           pixel_strip.setPixelColor(j, green);
          }
-          Serial.println("b");
-          pixel_strip.show();
-          buttonPushCounter = 1;
-          delay(250);
+         if (serialDebug) {
+           Serial.println("g");
+         }
+         pixel_strip.show();
+         delay(wait);
+  } 
+
+  if (buttonPushCounter == 3) {
+         for (j=0; j < 8; j++) {
+           pixel_strip.setPixelColor(j, blue);
+         }
+         if (serialDebug) {
+           Serial.println("b");
+         }
+         pixel_strip.show();
+         delay(wait);
+  } 
+  if (buttonPushCounter == 4) {
+         startUp();
+         if (serialDebug) {
+           Serial.println("startUp Flag");
+         }
+         pixel_strip.show();
+         buttonPushCounter = 0;
+         delay(wait);
   } 
 }
 
